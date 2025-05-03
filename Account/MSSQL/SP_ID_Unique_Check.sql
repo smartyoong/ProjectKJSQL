09f673a982f144ba2d7f72c1dc903eb9279efa6b
Kim Young Chan <smartyoong@naver.com>
2025-05-04 02:08:52 +0900

    모든 SP에 내용 보강 최적화 진행 앞으로 필요한 함수 및 시퀀스 내용 추가

--- Account/MSSQL/SP_ID_Unique_Check.sql
+++ Account/MSSQL/SP_ID_Unique_Check.sql
@@ -1,22 +1,25 @@
+USE [AccountDB]
+GO
+/****** Object:  StoredProcedure [dbo].[SP_ID_UNIQUE_CHECK]    Script Date: 2025-05-04 오전 2:06:54 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE PROCEDURE SP_ID_Unique_Check
+CREATE OR ALTER PROCEDURE [dbo].[SP_ID_UNIQUE_CHECK]
     @ID VARCHAR(50)
 AS
 BEGIN
+
+	SET LOCK_TIMEOUT 20000;
+
 	/*ID 중복 검사*/
     DECLARE @ErrorCode INT;
+	SET @ErrorCode = 0;
     SET @ErrorCode = (SELECT COUNT(*) FROM TBL_ACCOUNT WHERE ID = @ID);
 	IF @ErrorCode > 0
 	BEGIN
-		RETURN -1;
+		SET @ErrorCode = -1;
 	END
-    RETURN 0;
+    RETURN @ErrorCode;
 END
-GO
-
-
